e816594b610454c16a0aa36cfd1ee3667d5893bf
lore14th <lorenzo.fortina.95@gmail.com>
2022-04-27 10:59:02 +0200

    Update premake script

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,13 @@ project "glfw"
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
+
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+
+		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c"
 	}
@@ -26,6 +33,7 @@ project "glfw"
 		{
 			"src/win32_init.c",
 			"src/win32_joystick.c",
+			"src/win32_module.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
@@ -40,13 +48,18 @@ project "glfw"
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
-		
-		filter "configurations:Debug"
+
+	filter "configurations:Debug"
 		runtime "Debug"
-		symbols "On"	-- debug version --
+		symbols "On"
 
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "Full"	-- release version --
-		inlining "Auto"
-		floatingpoint "Fast"
+		optimize "On"
+		symbols "On"
+
+	filter "configurations:Distribution"
+		runtime "Release"
+		optimize "On"
+		symbols "Off"
+	
