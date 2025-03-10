fe179a2c570389d07135c603d86e712d3b15ef07
Lorenzo Fortina <lorenzo.fortina.95@gmail.com>
2025-03-10 10:19:02 +0100

    Update build script to use base class folder and file filtering

--- GLFW.Build.cs
+++ GLFW.Build.cs
@@ -1,71 +1,48 @@
+// GLFW Tinfoil build script
+
 using Sharpmake;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
 [Sharpmake.Generate]
-public class GLFW : Sharpmake.Project
+public class GLFW : TinfoilProjectBase
 {
 	public GLFW()
 	{
 		Name = "GLFW";
-		IsFileNameToLower = false;
-		IsTargetFileNameToLower = false;
-
 		SourceFiles.Add("GLFW.Build.cs");
-
-		AddTargets(new Target(
-			Platform.win64,
-			DevEnv.vs2022,
-			Optimization.Debug | Optimization.Release)
-		);
 	}
 
 	[Sharpmake.Configure]
-	public void ConfigureAll(Project.Configuration Config, Target Target)
+	public void ConfigureAll(Project.Configuration config, Target target)
 	{
-		Config.Output = Configuration.OutputType.Lib;
-		Config.ProjectPath = TinfoilSolution.GetProjectFileFolder();
-		Config.TargetPath = TinfoilSolution.GetBinaryFolder(Name, Target);
-		Config.IntermediatePath = TinfoilSolution.GetIntermediateFolder(Name, Target);
+		config.Output = Configuration.OutputType.Lib;
 
-		Config.Options.Add(Options.Vc.Compiler.CLanguageStandard.C11);
-		Config.Options.Add(Options.Vc.General.WindowsTargetPlatformVersion.Latest);
-		Config.Options.Add(Options.Vc.Librarian.TreatLibWarningAsErrors.Enable);
-		Config.Options.Add(new Options.Vc.Compiler.DisableSpecificWarnings("4244", "4100"));
-		// C4244: Cast possible data loss
-		// C4100: unused function param
+		config.Options.Add(Options.Vc.Compiler.CLanguageStandard.C11);
+		config.Options.Add(Options.Vc.General.WindowsTargetPlatformVersion.Latest);
+		config.Options.Add(Options.Vc.Librarian.TreatLibWarningAsErrors.Enable);
+		// C4244: Cast possible data loss, C4100: unused function param
+		config.Options.Add(new Options.Vc.Compiler.DisableSpecificWarnings("4244", "4100"));
 
-		Config.IncludePaths.Add("/include");
+		config.IncludePaths.Add("/include");
+		config.Defines.Add("_CRT_SECURE_NO_WARNINGS");
 
-		Config.Defines.Add("_CRT_SECURE_NO_WARNINGS");
+		ExcludeFolder(config, target, "deps");
+		ExcludeFolder(config, target, "examples");
+		ExcludeFolder(config, target, "tests");
 
-		List<string> ExcludedFilePrefixes = new List<string>();
-		if (Target.Platform == Platform.win64)
+		if (target.Platform == Platform.win64)
 		{
-			Config.Defines.Add("_GLFW_WIN32");
+			config.Defines.Add("_GLFW_WIN32");
 
-			ExcludedFilePrefixes.Add("cocoa");
-			ExcludedFilePrefixes.Add("linux");
-			ExcludedFilePrefixes.Add("posix");
-			ExcludedFilePrefixes.Add("wl");
-			ExcludedFilePrefixes.Add("x11");
-			ExcludedFilePrefixes.Add("xkb");
-			ExcludedFilePrefixes.Add("glx");
+			ExcludeFileByPrefix(config, target, "cocoa");
+			ExcludeFileByPrefix(config, target, "glx");
+			ExcludeFileByPrefix(config, target, "linux");
+			ExcludeFileByPrefix(config, target, "posix");
+			ExcludeFileByPrefix(config, target, "wl");
+			ExcludeFileByPrefix(config, target, "x11");
+			ExcludeFileByPrefix(config, target, "xkb");
 		}
-
-		List<string> ExcludedFolders = new List<string>();
-		ExcludedFolders.Add("deps");
-		ExcludedFolders.Add("examples");
-		ExcludedFolders.Add("tests");
-			
-		Config.SourceFilesBuildExcludeRegex.Add(@".*" + string.Join("|", ExcludedFilePrefixes.ToArray()) + @".*");
-		Config.SourceFilesBuildExcludeRegex.Add(@"\.*\/(" + string.Join("|", ExcludedFolders.ToArray()) + @")\/");
-	}
-
-	public static void LinkToProject(Project.Configuration ParentConfig, ITarget Target, string Folder, DependencySetting DependencySetting = DependencySetting.Default)
-	{
-		ParentConfig.AddPrivateDependency<GLFW>(Target, DependencySetting, Folder);
-		ParentConfig.IncludePaths.Add("/include");
 	}
 }
